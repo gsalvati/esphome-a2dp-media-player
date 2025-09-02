3904b6be92b239171e1fc6ccac10520585c66d8e
Gian Carlo Salvati <gian@salvati.com.br>
2025-09-02 00:08:58 -0300

    COmments

--- components/a2dp_media_player/__init__.py
+++ components/a2dp_media_player/__init__.py
@@ -5,14 +5,21 @@ from esphome.const import CONF_ID
 
 DEPENDENCIES = ['esp32_ble_tracker']
 
+# Definir o namespace e a classe do componente
 a2dp_media_player_ns = cg.esphome_ns.namespace('a2dp_media_player')
-A2DPMediaPlayer = a2dp_media_player_ns.class_('A2DPMediaPlayer', media_player.MediaPlayer, cg.Component)
+A2DPMediaPlayer = a2dp_media_player_ns.class_(
+    'A2DPMediaPlayer', media_player.MediaPlayer, cg.Component
+)
 
+# Schema de configuração
 CONFIG_SCHEMA = media_player.MEDIA_PLAYER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(A2DPMediaPlayer),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
+    # Criar uma variável para o componente
     var = cg.new_Pvariable(config[CONF_ID])
+    # Registrar como componente
     await cg.register_component(var, config)
+    # Registrar como media player
     await media_player.register_media_player(var, config)
