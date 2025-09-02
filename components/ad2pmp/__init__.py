5508ecc6c8ba3b4f48b996210c274c8c53c3f201
Gian Carlo Salvati <gian@salvati.com.br>
2025-09-02 01:08:11 -0300

    bring init back

---
+++ components/ad2pmp/__init__.py
@@ -0,0 +1,18 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import media_player
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ['esp32_ble_tracker']
+
+ad2pmp_ns = cg.esphome_ns.namespace('ad2pmp')
+A2DPMediaPlayer = ad2pmp_ns.class_('A2DPMediaPlayer', media_player.MediaPlayer, cg.Component)
+
+CONFIG_SCHEMA = media_player.MEDIA_PLAYER_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(A2DPMediaPlayer),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await media_player.register_media_player(var, config)
